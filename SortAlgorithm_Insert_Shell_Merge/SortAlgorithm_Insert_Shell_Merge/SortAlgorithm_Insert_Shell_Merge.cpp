e62b201785fac6c8383efe1c4c36c77561b29075
regan <xutaogeng@163.com>
2018-04-20 17:49:07 +0800

    list

--- SortAlgorithm_Insert_Shell_Merge/SortAlgorithm_Insert_Shell_Merge/SortAlgorithm_Insert_Shell_Merge.cpp
+++ SortAlgorithm_Insert_Shell_Merge/SortAlgorithm_Insert_Shell_Merge/SortAlgorithm_Insert_Shell_Merge.cpp
@@ -14,6 +14,24 @@ void PrintArray(int *data, int length)
 	printf("\n");
 }
 
+int InsertSort2(int *data, int length)
+{
+	int current;
+	for(int i=1; i<length; i++)
+	{
+		current = data[i];
+		int preIndex = i-1;
+		while(preIndex >=0 && data[preIndex]>current)
+		{
+			data[preIndex+1] = data[preIndex];
+			preIndex--;
+		}
+		data[preIndex+1] = current;
+		PrintArray(data,length);
+	}
+	return 0;
+}
+
 int InsertSort(int *data, int length)
 {
 	int current;
@@ -52,12 +70,15 @@ int ShellSort(int *data, int length)
 	return  0;
 }
 
+
 int main(int argc, char** argv)
 {
 	int data[10] = {1,38,5, 44, 15,36, 27, 2, 44, 19};
 
 	PrintArray(data, 10);
+	
 	printf("==========================================\n");
+	//InsertSort2(data, 10);
 	ShellSort(data, 10);
 
 	char c = getchar();
--- SortAlgorithm_maopao_select/SortAlgorithm_maopao_select/SortAlgorithm_maopao_select.cpp
+++ SortAlgorithm_maopao_select/SortAlgorithm_maopao_select/SortAlgorithm_maopao_select.cpp
@@ -13,8 +13,42 @@ void PrintArray(int *data, int length)
 	printf("\n");
 }
 
+void swap(int *data, int j, int k)
+{
+	int temp = data[j];
+	data[j] = data[k];
+	data[k] = temp;
+}
+
+
 int BubbleSort(int *data, int length)
 {
+	//Another method
+	for(int i=0; i<length-1; i++)
+	{
+		for(int j=length-2; j>=i; j--)
+		{
+			if(data[j] < data[j+1])
+			{
+				swap(data, j, j+1);
+			}
+		}
+		PrintArray(data, length);
+	}
+	/*
+	for(int i=0; i<length-1; i++)
+	{
+		for(int j=0; j<length-1-i; j++)
+		{
+			if(data[j] < data[j+1])
+			{
+				swap(data, j, j+1);
+			}
+		}
+		PrintArray(data, length);
+	}
+	*/
+	/*
 	for (int i=0; i<length; i++)
 	{
 		for (int j=0; j<length-1-i; j++)
@@ -28,7 +62,41 @@ int BubbleSort(int *data, int length)
 		}
 		PrintArray(data, length);
 	}
+	*/
+	return 0;
+}
+int SelectionSort3(int *data, int length)
+{
+	for(int i=0; i<length; i++)
+	{
+		int min = i;
+		for(int j=i+1; j<length; j++)
+		{
+			if(data[min] > data[j])
+				min = j;
+		}
 
+		if(min != i)
+			swap(data, min, i);
+		PrintArray(data, length);
+	}
+	return 0;
+}
+
+int SelectionSort2(int *data, int length)
+{
+	for(int i=0; i<length-1; i++)
+	{
+		int min = i;
+		for(int j=i; j<length; j++)
+		{
+			if(data[min] > data[j])
+				min = j;
+		}
+		if(min !=i)
+			swap(data, i, min);
+		PrintArray(data, length);
+	}
 	return 0;
 }
 
@@ -53,14 +121,42 @@ int SelectionSort(int *data, int length)
 	return 0;
 }
 
+void QuickSort(int *data, int _left, int _right)
+{
+	int left = _left;
+	int right = _right;
+	int temp = 0;
+	if(left <right)
+	{
+		temp = data[left];
+		while(left != right)
+		{
+			while(right>left && data[right]>= temp)
+				right--;
+			data[left] = data[right];
+
+			while(left<right && data[left]<=temp)
+				left++;
+			data[right] = data[left];
+		}
+		data[right] = temp;
+		QuickSort(data, _left, left-1);
+		QuickSort(data, right+1, _right);
+	}
+
+}
+
 int main(int argc, char** argv)
 {
-	int data[10] = {1,38,5, 44, 15,36, 27, 2, 44, 19};
+	//int data[10] = {1,38,5, 44, 15,36, 27, 2, 44, 19};
+	int data[10] = {9,3,1,4,2,0,7,8,6,5};
 
 	PrintArray(data, 10);
 	printf("==========================================\n");
 	//BubbleSort(data, sizeof(data)/sizeof(int));
-	SelectionSort(data, sizeof(data)/sizeof(int));
+	//SelectionSort3(data, sizeof(data)/sizeof(int));
+	QuickSort(data, 0, 9);
+	PrintArray(data, 10);
 	char c = getchar();
 
 	return 0;
